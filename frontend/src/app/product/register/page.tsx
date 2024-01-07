5608aaa5325eb0349fdd81622ba3e7eca045930c
benilton01 <bsa.ele@uea.edu.br>
2024-01-06 20:41:55 -0400

    feat: register book

--- frontend/src/app/product/register/page.tsx
+++ frontend/src/app/product/register/page.tsx
@@ -3,66 +3,233 @@
 import React, { useState } from 'react';
 
 export default function Page() {
-  const [productName, setProductName] = useState('');
-  const [price, setPrice] = useState('');
-  const [description, setDescription] = useState('');
+  const [bookName, setBookName] = useState('');
+  const [nomeDoAutor, setNomeDoAutor] = useState('')
+  const [tipo, setTipo] = useState('')
+  const [genero, setGenero] = useState('')
+  const [editora, setEditora] = useState('')
+  const [lancamento, setLancamento] = useState('')
+  const [anoDeEdicao, setAnoEdicao] = useState('')
+  const [numEdicao, setNumEdicao] = useState('')
+  const [loading, setLoading] = useState(false);
 
-  const handleProductRegistration = () => {
+
+  const handleProductRegistration = async () => {
     // Handle product registration logic here
-    console.log('Registering product with:', { productName, price, description });
-  };
+    console.log('Registering product with:', { 
+      bookName,
+      nomeDoAutor,
+      tipo,
+      genero,
+      editora,
+      lancamento,
+      anoDeEdicao,
+      numEdicao, });
 
+
+      try {
+        setLoading(true);
+        const postData = {
+          nome: bookName,
+          nomeDoAutor: nomeDoAutor,
+          lancamento: lancamento,
+          tipo: tipo,
+          genero: genero,
+          editora: editora,
+          anoEdicao: anoDeEdicao,
+          numEdicao: numEdicao
+        };
+    
+        // Limpar os estados dos campos
+        setBookName('');
+        setNomeDoAutor('');
+        setTipo('');
+        setGenero('');
+        setEditora('');
+        setLancamento('');
+        setAnoEdicao('');
+        setNumEdicao('');
+
+        const token = localStorage.getItem('accessToken');
+    
+        const response = await fetch('http://localhost:7070/books', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          },
+          body: JSON.stringify(postData)
+        });
+        setLoading(false);
+        if (response.ok) {
+          const data = await response.json();
+          console.log('Product registered successfully:', data);
+    
+        } else {
+          console.error('Failed to register product:', response.statusText);
+        }
+      } catch (error) {
+        setLoading(false);
+        console.error('Error registering product:', error);
+      } finally{
+        setLoading(false);
+      }
+    };
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="max-w-md w-full p-6 bg-gray-100 shadow-md">
-        <h2 className="text-2xl font-bold mb-6">Cadastrar Produto</h2>
+        <h2 className="text-2xl font-bold mb-6">Cadastrar Livro</h2>
         <form>
+         
+         
           <div className="mb-4">
-            <label htmlFor="productName" className="block text-gray-600 text-sm font-medium mb-2">
-              Product Name
+            <label htmlFor="bookName" className="block text-gray-600 text-sm font-medium mb-2">
+              Nome do Livro
             </label>
             <input
               type="text"
-              id="productName"
-              name="productName"
-              value={productName}
-              onChange={(e) => setProductName(e.target.value)}
+              id="bookName"
+              name="bookName"
+              value={bookName}
+              onChange={(e) => setBookName(e.target.value)}
               className="w-full p-2 border border-gray-300 rounded-md"
+              required
             />
           </div>
+          
+
           <div className="mb-4">
-            <label htmlFor="price" className="block text-gray-600 text-sm font-medium mb-2">
-              Price
-            </label>
-            <input
-              type="number"
-              id="price"
-              name="price"
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-              className="w-full p-2 border border-gray-300 rounded-md"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="description" className="block text-gray-600 text-sm font-medium mb-2">
-              Description
+            <label htmlFor="nomeDoAutor" className="block text-gray-600 text-sm font-medium mb-2">
+              Nome do Autor
             </label>
             <textarea
-              id="description"
-              name="description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
+              id="nomeDoAutor"
+              name="nomeDoAutor"
+              value={nomeDoAutor}
+              onChange={(e) => setNomeDoAutor(e.target.value)}
               className="w-full p-2 border border-gray-300 rounded-md resize-none"
+              required
             //   rows="4"
             ></textarea>
           </div>
+
+
+          <div className="mb-4">
+            <label htmlFor="tipo" className="block text-gray-600 text-sm font-medium mb-2">
+              Tipo
+            </label>
+            <input
+              type="text"
+              id="tipo"
+              name="tipo"
+              value={tipo}
+              onChange={(e) => setTipo(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded-md"
+              required
+            />
+          </div>
+
+
+          <div className="mb-4">
+            <label htmlFor="genero" className="block text-gray-600 text-sm font-medium mb-2">
+              Gênero
+            </label>
+            <input
+              type="text"
+              id="genero"
+              name="genero"
+              value={genero}
+              onChange={(e) => setGenero(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded-md"
+              required
+            />
+          </div>
+
+
+          <div className="mb-4">
+            <label htmlFor="editora" className="block text-gray-600 text-sm font-medium mb-2">
+              Editora
+            </label>
+            <input
+              type="text"
+              id="editora"
+              name="editora"
+              value={editora}
+              onChange={(e) => setEditora(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded-md"
+              required
+            />
+          </div>
+
+
+          <div className="mb-4">
+            <label htmlFor="lancamento" className="block text-gray-600 text-sm font-medium mb-2">
+              Lançamento
+            </label>
+            <input
+              type="number"
+              id="lancamento"
+              name="lancamento"
+              value={lancamento}
+              onChange={(e) => setLancamento(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded-md"
+              required
+            />
+          </div>
+
+
+          <div className="mb-4">
+            <label htmlFor="anoDeEdicao" className="block text-gray-600 text-sm font-medium mb-2">
+              Ano de edição
+            </label>
+            <input
+              type="number"
+              id="anoDeEdicao"
+              name="anoDeEdicao"
+              value={anoDeEdicao}
+              onChange={(e) => setAnoEdicao(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded-md"
+              required
+            />
+          </div>
+
+
+          <div className="mb-4">
+            <label htmlFor="numEdicao" className="block text-gray-600 text-sm font-medium mb-2">
+              Número da Edição
+            </label>
+            <input
+              type="number"
+              id="numEdicao"
+              name="numEdicao"
+              value={numEdicao}
+              onChange={(e) => setNumEdicao(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded-md"
+              required
+            />
+          </div>
+
           <button
+            type="button"
+            onClick={handleProductRegistration}
+            className="w-full bg-primary text-white p-2 rounded-md hover:bg-primary-dark relative"
+          >
+            {loading && (
+              <div className="absolute inset-0 flex items-center justify-center">
+                <div className="loader"></div>
+              </div>
+            )}
+            {loading ? 'Cadastrando...' : 'Cadastrar'}
+          </button>
+          
+          {/* <button
             type="button"
             onClick={handleProductRegistration}
             className="w-full bg-primary text-white p-2 rounded-md hover:bg-primary-dark"
           >
             Cadastar
-          </button>
+          </button> */}
         </form>
       </div>
     </div>
